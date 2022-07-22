c9d8f1f021b03b8940966bb3a146fbc5e97f93f1
Aleksei Tsysin <98952772+atsysin@users.noreply.github.com>
2022-07-22 13:52:52 +0300

    Deprecate exceptions

--- complex_rest_dtcd_supergraph/exceptions.py
+++ complex_rest_dtcd_supergraph/exceptions.py
@@ -5,14 +5,6 @@ Custom exceptions.
 from rest_framework.exceptions import APIException
 
 
-class FragmentDoesNotBelongToGraph(Exception):
-    """Fragment does not belong to given graph."""
-
-
-class FragmentIsNotBound(Exception):
-    """Fragment is unbound."""
-
-
 class LoadingError(APIException):
     """Failed to convert data to content subgraph."""
 
