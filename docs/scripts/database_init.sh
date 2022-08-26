c7cc03d7e27265e87fb7b43b184e62f38c7d2ff6
Aleksei Tsysin <98952772+atsysin@users.noreply.github.com>
2022-08-26 12:26:27 +0300

    Remove old scripts, update database init script

--- docs/scripts/database_init.sh
+++ docs/scripts/database_init.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# Creates the initial Root node and saves its UID 
-# into a text file "default_root_uid.txt" in plugin's directory.
+# Creates the initial Root node and re-installs constrains and indexes.
 
+# <project>/plugins/<this-plugin>/
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-# use this plugin's venv
+# go to project's directory
+cd $SCRIPT_DIR/../..
+
+# use project's venv
 python="venv/bin/python"
 
+# limit the loaded plugins to this particular one
+export COMPLEX_REST_PLUGIN_NAME="complex_rest_dtcd_supergraph"
+
 # re-install labels
-apps="models"
-conf="supergraph.conf"
-$python reinstall_labels.py $apps $conf
+$python complex_rest/manage.py install_labels --reset
 
 # initialize Root
-$python create_default_root.py
+$python complex_rest/manage.py create_default_root_node
