ace7079528b46a5187c5ea61af8dd8161ae6eeef
Nikita Serditov <nserditov@isgneuro.com>
2023-12-07 14:12:55 +0300

    made a proper url

--- complex_rest_dtcd_supergraph/urls.py
+++ complex_rest_dtcd_supergraph/urls.py
@@ -1,9 +1,9 @@
-from django.urls import re_path
+from django.urls import path
 
 from .views import GraphView
 
 app_name = "supergraph"
 
 urlpatterns = [
-    re_path(r'^complex_rest_dtcd_supergraph/v1/graph/?$', GraphView.as_view()),
+    path('graph/', GraphView.as_view()),
 ]
