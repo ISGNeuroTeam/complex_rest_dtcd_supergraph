f0a7425f2a80940c5d595bc9b94085fa9ebf3538
Nikita Serditov <nserditov@isgneuro.com>
2024-01-22 14:29:20 +0300

    testsa re passing

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,88 +1,50 @@
-import unittest
-from pathlib import Path
-from pprint import pformat
-from types import SimpleNamespace
+from random import randint
 
-import dictdiffer
-from django.urls import reverse
-from django.test import Client, tag
-from rest_framework import status
-from rest_framework.test import APISimpleTestCase
-
-from .misc import load_data, sort_payload
-
-TEST_DIR = Path(__file__).resolve().parent
-CLIENT = Client()
-
-# DEBUG
-DEBUG_FILEPATH = "debug.txt"
+from django.test import TestCase
+from rest_framework.test import APIClient
 
 
-class APITestCaseMixin:
-    """Some common attributes and methods for API tests.
+# current tests do run with running complex rest with attached supergraph plugin
+# so in order to run these tests user needs to run complex rest Django test suite
+# with current tests folder mentioned as a parameter
+class GraphViewTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
 
-    The idea: send an HTTP requests to a URL endpoint, get the response
-    and check the status.
-    """
+    def test_get(self):
+        response = self.client.get('/complex_rest_dtcd_supergraph/v1/graphs/')
+        self.assertEqual(response.status_code, 200)
 
-    expected_status = SimpleNamespace(
-        get=status.HTTP_200_OK,
-        post=status.HTTP_201_CREATED,
-        put=status.HTTP_200_OK,  # or 204; 201 if created
-        delete=status.HTTP_200_OK,  # 202 on queue, 204 on noDATA_DIR = TEST_DIR / "data"
-        URL_RESET=reverse("supergraph:reset")  # post here resets the db content
-    )
-    url = None  # the URL endpoint to check
-
-    def get(self):
-        """Send `GET` request to `self.url`, validate status and return
-        the response.
-        """
-
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, self.expected_status.get)
-        return response
-
-    def post(self, data: dict):
-        """Send `POST` request with given data in JSON format to
-        `self.url`, validate status and return the response.
-        """
-
-        response = self.client.post(self.url, data=data, format="json")
-        self.assertEqual(response.status_code, self.expected_status.post)
-        return response
-
-    def put(self, data: dict):
-        """Send `PUT` request with given data in JSON format to
-        `self.url`, validate status and return the response.
-        """
-
-        response = self.client.put(self.url, data=data, format="json")
-        self.assertEqual(response.status_code, self.expected_status.put)
-        return response
-
-    def delete(self):
-        """Send `DELETE` request to `self.url`, validate status and return
-        the response.
-        """
-
-        response = self.client.delete(self.url)
-        self.assertEqual(response.status_code, self.expected_status.delete)
-        return response
-
-    def _test_delete(self):
-        """Simple implementation for delete endpoint.
-
-        Calls `.delete()`, then gets the resource on the same URL and
-        checks the status to be 404.
-        """
-
-        r1 = self.delete()
-        r2 = self.client.get(self.url)  # note explicit request
-        self.assertEqual(r2.status_code, status.HTTP_404_NOT_FOUND)
-
-        return r1, r2
+    def test_post(self):
+        data = {'graph': 'some graph data'}
+        response = self.client.post('/complex_rest_dtcd_supergraph/v1/graphs/', data)
+        self.assertEqual(response.status_code, 400)
+        random: int = randint(0, 1000)
+        data = {'graph': 'some graph data', 'title': f'some-new-graph-{random}'}
+        response = self.client.post('/complex_rest_dtcd_supergraph/v1/graphs/', data)
+        self.assertEqual(response.status_code, 200)
+        self.graph_id = response.data['result']['graph_id']
+        # clean up
+        self.client.delete(f'/complex_rest_dtcd_supergraph/v1/graphs/{self.graph_id}/')
 
 
-if __name__ == "__main__":
-    unittest.main()
+class GraphDetailViewTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_all(self) -> None:
+        #     create test graph
+        random: int = randint(0, 1000)
+        data = {'graph': 'some graph data', 'title': f'some-new-graph-{random}'}
+        response = self.client.post('/complex_rest_dtcd_supergraph/v1/graphs/', data)
+        self.graph_id = response.data['result']['graph_id']
+        # test get
+        response = self.client.get(f'/complex_rest_dtcd_supergraph/v1/graphs/{self.graph_id}/')
+        self.assertEqual(response.status_code, 200)
+        # test post
+        data = {'graph': 'updated graph data'}
+        response = self.client.put(f'/complex_rest_dtcd_supergraph/v1/graphs/{self.graph_id}/', data)
+        self.assertEqual(response.status_code, 200)
+         # test delete
+        response = self.client.delete(f'/complex_rest_dtcd_supergraph/v1/graphs/{self.graph_id}/')
+        self.assertEqual(response.status_code, 200)
